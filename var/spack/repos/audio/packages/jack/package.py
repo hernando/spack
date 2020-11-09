b3285bd60d663751540ef985c2f26630989ec43c
Juan Hernando <juan.hernando@pix4d.com>
2020-11-09 20:40:20 +0100

    Add jackdbus support to JACK package

--- var/spack/repos/audio/packages/jack/package.py
+++ var/spack/repos/audio/packages/jack/package.py
@@ -8,7 +8,16 @@ class Jack(WafPackage):
     version('1.9.14', tag='v1.9.14')
 
     variant('alsa', default=True)
+    variant('dbus', default=True)
 
     depends_on('alsa', when='+alsa')
+    depends_on('dbus', when='+dbus')
     depends_on('samplerate')
+    depends_on('libsndfile')
     depends_on('readline')
+
+    def configure_args(self):
+
+        if '+dbus' in self.spec:
+            return ["--dbus", "--classic"]
+        return []
--- var/spack/repos/audio/packages/qjackctl/package.py
+++ var/spack/repos/audio/packages/qjackctl/package.py
@@ -8,5 +8,5 @@ class Qjackctl(AutotoolsPackage):
     version('0.6.2', sha256="1ec77d0e8edac1b4d60a32a08d2f4329f90571801920cb48c6147e0eae6f50e6")
 
     depends_on("qt@5.12: +dbus +opengl")
-    depends_on("jack", type=("build", "link", "run"))
-
+    depends_on("alsa")
+    depends_on("jack +dbus", type=("build", "link", "run"))
